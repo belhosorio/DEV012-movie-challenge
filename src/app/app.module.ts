f8d8a94636ecf1b510ebb74cd84bd42c345e3ae6
belh osorio <belh.osoriocom@gmail.com>
2024-01-29 18:03:32 -0300

    creacion de componentes home, movie-detail, logo y filter, y avance en scss de vista home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './views/home/home.component';
+import { MovieDetailComponent } from './views/movie-detail/movie-detail.component';
+import { FiltersComponent } from './components/filters/filters.component';
+import { LogoComponent } from './components/logo/logo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    MovieDetailComponent,
+    FiltersComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule,
